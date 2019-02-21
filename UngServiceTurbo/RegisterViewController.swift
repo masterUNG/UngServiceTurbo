d8bbac5cc5a964630cb8d2e5bcb9d82fe673a47f
masterUNG <phrombutr@gmail.com>
2019-02-21 13:23:39 +0700

    Check Space and Alert Success

--- UngServiceTurbo/RegisterViewController.swift
+++ UngServiceTurbo/RegisterViewController.swift
@@ -9,22 +9,47 @@
 import UIKit
 
 class RegisterViewController: UIViewController {
+    
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var userTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
-    }
+    }   // Method Main
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func uploadButton(_ sender: UIBarButtonItem) {
+        
+        let name: String = nameTextField.text!
+        let user: String = userTextField.text!
+        let password: String = passwordTextField.text!
+        
+        print("name ==> \(name),  user ==> \(user), password ==> \(password)")
+        if (name.count == 0) || (user.count == 0) || (password.count == 0){
+//            Have Space
+            registerAlert(title: "Have Space", message: "Please Fill All Blank")
+        } else {
+//            No Space
+            
+        }   // if
+    }   //uploadButton
+    
+    func registerAlert(title: String, message: String) -> Void {
+        
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }))
+        present(alert, animated: true, completion: nil)
+        
+        
     }
-    */
 
-}
+
+}   // Main Class
